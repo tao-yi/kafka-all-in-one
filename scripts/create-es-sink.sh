e787b5a7ef642ecd9fa23945fdacbc0fced9fe6d
taoyi <tao.yi@treelab.co>
2021-04-28 18:42:44 +0800

    fix: scripts

--- scripts/create-es-sink.sh
+++ scripts/create-es-sink.sh
@@ -2,13 +2,4 @@
 
 curl -X "POST" "http://localhost:28084/connectors" \
 		-H "Content-Type: application/json" \
-		-d '{ 
-          "name": "quickstart-elasticsearch-source",  
-          "config": {
-            "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
-            "connection.url":"http://elasticsearch:9200",
-						"tasks.max": 1,
-            "topics": "quickstart-mysql-topic",
-            "poll.interval.ms": 1000
-						}
-				}' 
+		-d @"es-sink.json"
--- scripts/update-es-sink.sh
+++ scripts/update-es-sink.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 
-curl -X PUT "http://localhost:28084/connectors/quickstart-elasticsearch-source/config" \
+curl -X PUT "http://localhost:28084/connectors/quickstart-elasticsearch-sink/config" \
 		-H "Content-Type: application/json" \
-		-d '{ 
-          "connector.class": "io.confluent.connect.elasticsearch.ElasticsearchSinkConnector",
-          "connection.url":"http://elasticsearch:9200",
-          "tasks.max": 1,
-          "topics": "quickstart-mysql-topic",
-          "poll.interval.ms": 1000
-				}' 
+		-d @"es-connect-config.json"
+
+curl -X PUT "http://localhost:28083/connectors/quickstart-mysql-source/config" \
+		-H "Content-Type: application/json" \
+		-d @"mysql-connect-config.json"
